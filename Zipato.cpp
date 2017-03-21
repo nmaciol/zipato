d486dd8649daa4af779c0e6af09cd1031cea6179
nmaciol <m76013@googlemail.com>
2017-03-21 09:23:05 +0100

    Add files via upload

---
+++ Zipato.cpp
@@ -0,0 +1,115 @@
+#include "Zipato.h"
+
+
+Zipato::Zipato (String host, String username, String password) {
+  
+    _host        = host;
+    _username    = username;
+    _password    = password;
+    
+}
+
+String Zipato::getAttribtValue(String attribut) {
+    
+    StaticJsonBuffer<512>   jsonBuffer;
+ 
+    String url = "http://" + (String)(_host) + "/zipato-web/v2/attributes/" + attribut + "/value";
+
+    if (!this->get(url)) { return "";  }
+
+    JsonObject& root = jsonBuffer.parseObject(_payload);
+
+    if (root.containsKey("value") ){
+        return root["value"].asString();
+    } else{
+        root.prettyPrintTo(Serial);
+        return "";
+    }
+     
+}
+
+
+
+
+
+bool Zipato::get(String url){
+
+        _http.setReuse(true);
+        _http.begin(url);
+     
+        if( _jsessionid != "" ) {  _http.addHeader("Cookie", "JSESSIONID=" + _jsessionid); }
+        int httpCode = _http.GET();
+
+        if(httpCode > 0) {
+
+            if(httpCode == HTTP_CODE_OK) {
+
+                _payload = _http.getString();
+                return true;
+
+            } else {
+                // Http  Status Code != 200
+                _errorText =  "GET - HTTP StatusCode = " + httpCode;
+            }
+            
+        } else {
+            // Http connection 
+            _errorText =  _http.errorToString(httpCode).c_str();
+        }
+        _http.end();
+        return false;
+
+        
+}
+
+String Zipato::getErrorText(){
+
+
+        if((_errorText).length() == 0){
+
+            return _payload;
+           
+        } else {
+
+            return _errorText;
+        }
+}
+
+
+bool Zipato::login() {
+        StaticJsonBuffer<512>   jsonBuffer;     
+        
+        //    1 1 1 1 1 1 1 1 1
+                     
+        String url = "http://" + (String)(_host) + "/zipato-web/v2/user/init";
+        _jsessionid = "";
+        if (!this->get(url)) { return false;  }
+
+        JsonObject& root = jsonBuffer.parseObject(_payload);
+
+        _jsessionid = root["jsessionid"].asString();
+                
+        String token = sha1(root["nonce"].asString() + sha1(_password));
+     
+        //     2 2 2 2 2 2 2 2 2
+                
+        url = "http://" + (String)(_host) + "/zipato-web/v2/user/login?username=" + _username + "&token=" + token;
+        if (!this->get(url)) { return false; }
+
+        JsonObject& root2 = jsonBuffer.parseObject(_payload);
+
+        if (strcmp( root2["success"].asString(), "true" ) != 0 ) {return false;}
+       
+        return true;
+
+}
+
+bool Zipato::logout() {
+       
+        String url = "http://" + (String)(_host) + "/zipato-web/v2/user/logout";
+
+        if (!this->get(url)) { return false;  }
+
+        return true;
+}
+
---
+++ Zipato.h
@@ -0,0 +1,42 @@
+
+#ifndef Zipato_H
+#define Zipato_H
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h> 
+#include <WiFiClientSecure.h>
+#include <ESP8266HTTPClient.h>
+#include <Hash.h>
+
+#include "Arduino.h"
+
+class Zipato {
+
+    public:
+        // Constructor 
+        Zipato (String host, String username, String password );
+        
+        
+        bool login();
+        bool logout();
+        
+        String getErrorText();
+        String getAttribtValue(String attribut);
+
+    private:
+        String _jsessionid;
+        String _host;
+        String _username;
+        String _password; 
+        String _payload;
+        String _errorText;
+        
+        HTTPClient _http;
+
+ 
+        bool get(String url);
+    
+    
+       
+};
+
+#endif
